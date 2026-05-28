461850aa34bd378060650801f75d41f91182dbcb
sujith kumar <148097955+sujith52@users.noreply.github.com>
2026-05-28 22:50:09 +0530

    Add files via upload

---
+++ may28/CalcTest.java
@@ -0,0 +1,14 @@
+package com.sujith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalcTest {
+
+	@Test
+	void test() {
+		System.out.println("This is the running of the junit in the first case case of senerio!");
+	}
+
+}
---
+++ may28/Demo.java
@@ -0,0 +1,11 @@
+package may28;
+
+public class Demo {
+    public int divide(int a, int b){
+        return a/b;
+    }
+    public int multiply(int a, int b){
+        return a*b;
+    }
+    
+}
